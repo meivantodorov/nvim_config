22eb48c20016e631b8d8449fc91fb60dee7ce110
Ivan Todorov <meivantodorov@gmail.com>
2023-11-28 16:25:06 +0200

    Packer update

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>") 
+vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<cr>") 
+vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<cr>") 
 vim.keymap.set('n', '<C-p', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function() 
-	builtin.grep_string( { search = vim.fn.input("Grep > ") });
+vim.keymap.set('n', '<leader>fs', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
--- lua/meivantodorov/packer.lua
+++ lua/meivantodorov/packer.lua
@@ -1,43 +1,81 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   use {
-  	'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	-- or                            , branch = '0.1.x',
-	requires = { {'nvim-lua/plenary.nvim'} }  
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  require('packer').startup(function(use)
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-  end)
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
 
-  -- LPS ZERO
+  use({
+      "folke/trouble.nvim",
+      config = function()
+          require("trouble").setup {
+              icons = false,
+              -- your configuration comes here
+              -- or leave it empty to use the default settings
+              -- refer to the configuration section below
+          }
+      end
+  })
+
   use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    --- Uncomment these if you want to manage LSP servers from neovim
-    -- {'williamboman/mason.nvim'},
-    -- {'williamboman/mason-lspconfig.nvim'},
+			'nvim-treesitter/nvim-treesitter',
+			run = function()
+				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+				ts_update()
+			end,}
+  use("nvim-treesitter/playground")
+  use("theprimeagen/harpoon")
+  use("theprimeagen/refactoring.nvim")
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
+  use("nvim-treesitter/nvim-treesitter-context");
 
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  --{'williamboman/mason.nvim'},
+		  --{'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+	  }
   }
-}
+  use("christoomey/vim-tmux-navigator")
+  use("numToStr/Comment.nvim")
+  use("nvim-lualine/lualine.nvim")
+
+  --use("folke/zen-mode.nvim")
+  --use("github/copilot.vim")
+  --use("eandrju/cellular-automaton.nvim")
+  --use("laytan/cloak.nvim")
+
 end)
 
